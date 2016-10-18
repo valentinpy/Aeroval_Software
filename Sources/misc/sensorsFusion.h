231f193e41b1cb5c481142ac769d7648bca9ae69
Valentin Py <valentin.py@gmail.com>
2016-10-18 17:14:51 +0200

    Add gloabal variables for sensor fusion (read below)
    This is really awfull! But this is how Madgwick did! Whould be modified!

--- Sources/misc/sensorsFusion.h
+++ Sources/misc/sensorsFusion.h
@@ -20,6 +20,9 @@
 #ifndef SOURCES_MISC_SENSORSFUSION_H_
 #define SOURCES_MISC_SENSORSFUSION_H_
 
+extern volatile float beta;				// algorithm gain
+extern volatile float q0, q1, q2, q3;	// quaternion of sensor frame relative to auxiliary frame
+
 void MadgwickAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
 void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az);
 
